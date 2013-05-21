25ed8944730773689274ae444d76cba54e7ae81c
James McKinney <james@slashpoundbang.com>
2013-05-21 14:21:38 -0400

    rename databases, add ENV['SECRET_TOKEN'], remove comments from routes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ComingElections::Application.config.session_store :cookie_store, key: '_comingelections_session'
+ComingElections::Application.config.session_store :cookie_store, key: '_coming_elections_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
