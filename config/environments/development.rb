21568d68a3a26887fa9ea582234427bf0879facb
James McKinney <james@slashpoundbang.com>
2013-05-26 18:29:12 -0400

    don't send email in development, remove unnecessary copy-paste from development.rb to production.rb, use same code as heroku docs, require csv in election.rb instead

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,18 +34,4 @@ ComingElections::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  #actionmailer setup for gmail account
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   :location => '/usr/sbin/sendmail',
-  #   :arguments => '-i -t'
-  # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.delivery_method = :smtp
-
-
 end
