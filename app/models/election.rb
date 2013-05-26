d917107b0461639840dc839207d26f9c55c1eefb
James McKinney <james@slashpoundbang.com>
2013-05-26 18:43:48 -0400

    add pending specs for election

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,22 +1,24 @@
+require 'csv'
+
 class Election < ActiveRecord::Base
 
   attr_accessible :year, :start_date, :end_date, :jurisdiction, :division, :election_type, :scope, :notes, :source
 
   attr_accessor :date
 
-  before_save :set_dates 
-
+  before_save :set_year
+  before_save :set_end_date
 
   def self.create_or_update(attributes)
-    criteria = attributes.slice(:start_date,:jurisdiction,:election_type)   
+    criteria = attributes.slice(:start_date, :jurisdiction, :election_type)
     election = Election.where(criteria).first_or_initialize
-    election.assign_attributes(attributes.slice(:year,:end_date,:scope,:division,:notes,:source))
+    election.assign_attributes(attributes.slice(:year, :end_date, :scope, :division, :notes, :source))
     election.save!
   end
 
-  def self.to_csv 
-    @col_sep = ',' 
-    CSV.generate(:col_sep => @col_sep, :row_sep => "\r\n",:headers => :first_row) do |csv|
+  def self.to_csv
+    @col_sep = ','
+    CSV.generate(col_sep: @col_sep, row_sep: "\r\n", headers: :first_row) do |csv|
       csv << ["id","year","start_date","end_date","jurisdiction","division","election_type","scope","notes","source"]
       self.all.each do |election|
         begin
@@ -30,12 +32,17 @@ class Election < ActiveRecord::Base
 
 private
 
+  def set_year
+    if start_date_changed?
+      self.year = start_date.year
+    end
+  end
 
-  def set_dates
-    if start_date
-      self.year = self.start_date.year
-      self.end_date = self.start_date
+  def set_end_date
+    if start_date_changed?
+      if end_date.blank? || end_date == start_date_was
+        self.end_date = start_date
+      end
     end
   end
 end
-
---
+++ spec/models/election_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Election do
+  context 'when creating a record' do
+    it 'should set the year to match the start date' do
+      pending
+    end
+
+    it 'should set the end date to match the start date if the end date is empty' do
+      pending
+    end
+  end
+
+  context 'when updating a record' do
+    it 'should set the year to match the start date' do
+      pending
+    end
+
+    it 'should set the end date to match the start date if the end date is empty' do
+      pending
+    end
+
+    it 'should set the end date to match the start date if the end date was equal to the start date' do
+      pending
+    end
+
+    it 'should not set the end date to match the start date if the end date was different from the start date' do
+      pending
+    end
+  end
+end
