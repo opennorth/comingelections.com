279ccde6d8a215ca11344eb65b6045abcde8dea7
James McKinney <james@slashpoundbang.com>
2013-05-30 21:22:04 -0400

    move specs to past

--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Election do
   let :attributes do
     {
-      start_date: Date.new(2020, 1, 1),
+      start_date: Date.new(1920, 1, 1),
       jurisdiction: 'Canada',
       election_type: 'general',
       source: 'http://example.com/',
@@ -12,11 +12,11 @@ describe Election do
 
   describe '#within' do
     it 'should return the elections within the range' do
-      a = Election.create(attributes.merge(start_date: Date.new(2050, 1, 1)))
-      b = Election.create(attributes.merge(start_date: Date.new(2000, 1, 1)))
+      a = Election.create(attributes.merge(start_date: Date.new(1950, 1, 1)))
+      b = Election.create(attributes.merge(start_date: Date.new(1900, 1, 1)))
       c = Election.create(attributes)
       d = Election.create(attributes)
-      Election.within(Date.new(2010)..Date.new(2030)).should == [c, d]
+      Election.within(Date.new(1910)..Date.new(1930)).should == [c, d]
     end
   end
 
@@ -58,23 +58,23 @@ describe Election do
     end
 
     let :election_with_end_date do
-      Election.create(attributes.merge(end_date: Date.new(2020, 1, 2)))
+      Election.create(attributes.merge(end_date: Date.new(1920, 1, 2)))
     end
 
     context 'when creating a record' do
       it 'should set the year to match the start date' do
-        election.year.should == 2020
+        election.year.should == 1920
       end
 
       it 'should set the end date to match the start date if the end date is empty' do
-        election.end_date.should == Date.new(2020, 1, 1)
+        election.end_date.should == Date.new(1920, 1, 1)
       end
       it 'should not set the end date to match the start date if the end date is not empty' do
-        election_with_end_date.end_date.should == Date.new(2020, 1, 2)
+        election_with_end_date.end_date.should == Date.new(1920, 1, 2)
       end
 
       it 'should require the end date to be after the start date' do
-        election = Election.create(attributes.merge(end_date: Date.new(1990, 1, 1)))
+        election = Election.create(attributes.merge(end_date: Date.new(1900, 1, 1)))
         election.should have(1).error_on(:end_date)
       end
 
@@ -86,30 +86,30 @@ describe Election do
 
     context 'when updating a record' do
       it 'should update the year to match the start date if changed' do
-        election.update_attributes(start_date: Date.new(2025, 1, 1))
-        election.year.should == 2025
+        election.update_attributes(start_date: Date.new(1925, 1, 1))
+        election.year.should == 1925
       end
       it 'should not update the year to match the start date if not changed' do
         election.update_attributes(notes: 'Foo')
-        election.year.should == 2020
+        election.year.should == 1920
       end
 
       it 'should update the end date to match the start date if changed' do
-        election.update_attributes(start_date: Date.new(2025, 1, 1))
-        election.end_date.should == Date.new(2025, 1, 1)
+        election.update_attributes(start_date: Date.new(1925, 1, 1))
+        election.end_date.should == Date.new(1925, 1, 1)
       end
       it 'should not update the end date to match the start date if not changed' do
         election.update_attributes(notes: 'Foo')
-        election.end_date.should == Date.new(2020, 1, 1)
+        election.end_date.should == Date.new(1920, 1, 1)
       end
 
       it 'should update the end date to match the start date if the end date was equal to the start date' do
-        election.update_attributes(start_date: Date.new(2025, 1, 1))
-        election.end_date.should == Date.new(2025, 1, 1)
+        election.update_attributes(start_date: Date.new(1925, 1, 1))
+        election.end_date.should == Date.new(1925, 1, 1)
       end
       it 'should not update the end date to match the start date if the end date was not equal to the start date' do
-        election.update_attributes(start_date: Date.new(2025, 1, 1))
-        election_with_end_date.end_date.should == Date.new(2020, 1, 2)
+        election.update_attributes(start_date: Date.new(1925, 1, 1))
+        election_with_end_date.end_date.should == Date.new(1920, 1, 2)
       end
     end
   end
