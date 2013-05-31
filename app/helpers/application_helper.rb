19762f4c889a0e89eef4692d87fef343f19a5bbb
James McKinney <james@slashpoundbang.com>
2013-05-30 21:24:08 -0400

    move helper into view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,18 +3,9 @@ module ApplicationHelper
   MONTHS = %w[January February March April May June July August September October November December]
   SUFFIXES = %w[st nd rd th]
 
-
-  def info(election)
-    info = ''
-    info << election.attributes.values_at("jurisdiction", "election_type").join(' ')
-    info << ', '+election.division if election.division 
-    info
-  end
-
-
   def schedule_info(election)
     string = ''
-    if election.rank > 0  
+    if election.rank > 0
       string << "the #{election.rank}#{SUFFIXES[election.rank-1]} "
     else
       string << "the last"
@@ -24,5 +15,4 @@ module ApplicationHelper
     string << "every #{election.term_length} years"
     string
   end
-
 end
