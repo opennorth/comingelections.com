b0d4534a3f74a71480ae26ae7a6fff377dd1f5be
James McKinney <james@slashpoundbang.com>
2013-05-30 20:18:01 -0400

    add more election validations, use more literals in election spec

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,23 +1,50 @@
+# coding: utf-8
 require 'csv'
 
 class Election < ActiveRecord::Base
-  attr_accessible :year, :start_date, :end_date, :jurisdiction, :division, :election_type, :scope, :notes, :source
+  attr_accessible :year, :start_date, :end_date, :jurisdiction, :election_type, :division, :scope, :notes, :source
 
   before_validation :set_year, :set_end_date
 
   validates_presence_of :year, :start_date, :end_date, :jurisdiction, :election_type, :source
-  validates_presence_of :division, :if => :by_election?
-  validate :validate_dates
+  validates_presence_of :division, if: :by_election?
+  validates_inclusion_of :jurisdiction, in: [
+    'Canada',
+    # Provinces
+    'Alberta',
+    'British Columbia',
+    'Manitoba',
+    'New Brunswick',
+    'Newfoundland and Labrador',
+    'Nova Scotia',
+    'Ontario',
+    'Prince Edward Island',
+    'Quebec',
+    'Saskatchewan',
+    # Territories
+    'Northwest Territories',
+    'Nunavut',
+    'Yukon',
+  ]
+  validates_inclusion_of :election_type, in: %w(general by-election municipal)
+  validate :end_date_must_be_after_start_date
 
+  # @param [Integer] seconds seconds
+  def self.within_next(seconds)
+    where(start_date: Date.today..seconds.from_now.to_date)
+  end
+
+  # @param [Hash] attributes attributes
   def self.create_or_update(attributes)
     criteria = attributes.slice(:start_date, :jurisdiction, :election_type)
     election = Election.where(criteria).first_or_initialize
-    election.assign_attributes(attributes.slice(:year, :end_date, :scope, :division, :notes, :source))
-    election.save
+    election.assign_attributes(attributes)
+    election.save!
   end
 
+  # @return [String] the list of elections as a CSV file
   def self.to_csv
-    CSV.generate(row_sep: "\r\n", headers: :first_row) do |csv|
+    CSV.generate(row_sep: "\r\n") do |csv|
       csv << ['id', 'year', 'start_date', 'end_date', 'jurisdiction', 'division', 'election_type', 'scope', 'notes', 'source']
       all.each do |election|
         csv << election.attributes.values_at(:id, :year, :start_date, :end_date, :jurisdiction, :division, :election_type, :scope, :notes, :source)
@@ -41,11 +68,13 @@ private
     end
   end
 
-  def validate_dates
-    errors.add("End Date", "is invalid") if end_date < start_date
+  def end_date_must_be_after_start_date
+    if end_date < start_date
+      errors.add(:end_date, 'must be after start date')
+    end
   end
 
   def by_election?
-      election_type == "by-election"
+    election_type == "by-election"
   end
 end
--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -3,94 +3,94 @@ require 'spec_helper'
 describe Election do
   let :attributes do
     {
-      :start_date => Date.parse("October 1, 2015"),
-      :jurisdiction => "Ontario",
-      :election_type => "Provincial",
-      :scope => '',
-      :source => 'http://www.psc-cfp.gc.ca/plac-acpl/leave-conge/ann2-eng.htm',
+      start_date: Date.new(2020, 1, 1),
+      jurisdiction: 'Canada',
+      election_type: 'general',
+      source: 'http://example.com/',
     }
   end
 
-  context 'when creating a record' do
+  describe '#create_or_update' do
     it 'should create a new record' do
-      lambda{
+      expect{
         Election.create_or_update(attributes)
-      }.should change(Election, :count).by(1)
+      }.to change(Election, :count).by(1)
     end
 
-    it 'should set the year to match the start date' do
-      Election.create_or_update(attributes)
-      Election.last.year.should == attributes[:start_date].year
+    it 'should update a record if a match is found' do
+      expect{
+        election = Election.create(attributes)
+        Election.create_or_update(attributes.merge(notes: 'Foo'))
+      }.to change(Election, :count).by(1)
     end
 
-    it 'should set the end date to match the start date if the end date is empty' do
-      Election.create_or_update(attributes)
-      Election.last.end_date.should == attributes[:start_date]
+    it 'should not update a record if a match is not found' do
+      expect{
+        election = Election.create(attributes)
+        Election.create_or_update(attributes.merge(election_type: 'municipal'))
+      }.to change(Election, :count).by(2)
     end
-
-    it 'should not allow end_date to be before start_date' do
-      lambda{
-        Election.create_or_update(attributes.merge({:end_date => Date.parse("September 1, 2015")}))
-      }.should_not change(Election, :count)
-    end
-
-    it 'should ensure division is present if type is by-election' do
-      attributes[:election_type] = 'by-election'
-      lambda{
-        Election.create_or_update(attributes)
-      }.should_not change(Election, :count)
-    end
-
   end
 
-  context 'when updating a record' do
-    let :extra_info do
-      {
-        :scope => 'sample scope',
-        :notes => 'here is some sample info'
-      }
+  describe 'callbacks' do
+    let :election do
+      Election.create(attributes)
     end
 
-    before do
-      Election.create_or_update(attributes)
+    let :election_with_end_date do
+      Election.create(attributes.merge(end_date: Date.new(2020, 1, 2)))
     end
 
-    it 'should not create a new record' do
-      lambda{
-        Election.create_or_update(attributes.merge(extra_info))
-      }.should_not change(Election, :count)
+    context 'when creating a record' do
+      it 'should set the year to match the start date' do
+        election.year.should == 2020
+      end
+
+      it 'should set the end date to match the start date if the end date is empty' do
+        election.end_date.should == Date.new(2020, 1, 1)
+      end
+      it 'should not set the end date to match the start date if the end date is not empty' do
+        election_with_end_date.end_date.should == Date.new(2020, 1, 2)
+      end
+
+      it 'should require the end date to be after the start date' do
+        election = Election.create(attributes.merge(end_date: Date.new(1990, 1, 1)))
+        election.should have(1).error_on(:end_date)
+      end
+
+      it 'should require the division to be present if it is a by-election' do
+        election = Election.create(attributes.merge(election_type: 'by-election'))
+        election.should have(1).error_on(:division)
+      end
     end
 
-    it 'should set the year to match the start date' do
-      Election.create_or_update(attributes.merge(extra_info))
-      revised = Election.order("updated_at").last
-      revised.year.should == attributes[:start_date].year
-    end
+    context 'when updating a record' do
+      it 'should update the year to match the start date if changed' do
+        election.update_attributes(start_date: Date.new(2025, 1, 1))
+        election.year.should == 2025
+      end
+      it 'should not update the year to match the start date if not changed' do
+        election.update_attributes(notes: 'Foo')
+        election.year.should == 2020
+      end
 
-    it 'should set the end date to match the start date if the end date is empty' do
-      Election.create_or_update(attributes)
-      revised = Election.order("updated_at").last
-      revised.end_date.should == attributes[:start_date]
-    end
+      it 'should update the end date to match the start date if changed' do
+        election.update_attributes(start_date: Date.new(2025, 1, 1))
+        election.end_date.should == Date.new(2025, 1, 1)
+      end
+      it 'should not update the end date to match the start date if not changed' do
+        election.update_attributes(notes: 'Foo')
+        election.end_date.should == Date.new(2020, 1, 1)
+      end
 
-    it 'should set the end date to match the start date if the end date was equal to the start date' do
-      Election.create_or_update(attributes.merge({:end_date => Date.parse("October 1, 2015")}))
-      revised = Election.order("updated_at").last
-      revised.end_date.should == attributes[:start_date]
+      it 'should update the end date to match the start date if the end date was equal to the start date' do
+        election.update_attributes(start_date: Date.new(2025, 1, 1))
+        election.end_date.should == Date.new(2025, 1, 1)
+      end
+      it 'should not update the end date to match the start date if the end date was not equal to the start date' do
+        election.update_attributes(start_date: Date.new(2025, 1, 1))
+        election_with_end_date.end_date.should == Date.new(2020, 1, 2)
+      end
     end
-
-    it 'should not set the end date to match the start date if the end date was different from the start date' do
-      Election.create_or_update(attributes.merge({:end_date => Date.parse("October 3, 2015")}))
-      revised = Election.order("updated_at").last
-      revised.end_date.should == Date.parse("October 3, 2015")
-    end
-
-    it 'should update fields that are different' do
-      Election.create_or_update(attributes.merge(extra_info))
-      revised = Election.order("updated_at").last
-      revised.scope.should == extra_info[:scope]
-      revised.notes.should == extra_info[:notes]
-    end
-
   end
 end
